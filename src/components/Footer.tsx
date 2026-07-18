2ebba5f658b4983a7e7e43cdcf7b81d73622c664
Josh Glazer <joshglazer@gmail.com>
2026-07-18 11:10:52 -0400

    Add listening-delay disclaimer to footer (#33)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,37 +1,53 @@
 import { SiGithub } from "react-icons/si";
+import { auth } from "@/auth";
 import { Text } from "@/design/atoms/Text";
 import { Link } from "@/design/atoms/Link";
 import { HStack } from "@/design/atoms/Stack";
 
-export function Footer() {
+export async function Footer() {
+  const session = await auth();
+
   return (
-    <footer className="flex items-center justify-between border-t border-zinc-200 px-6 py-4 dark:border-zinc-800">
-      <Text type="supporting" size="sm">
-        A{" "}
-        <Link
-          href="https://joshglazer.com"
-          target="_blank"
-          type="inherit"
-          color="inherit"
-        >
-          Josh Glazer
-        </Link>{" "}
-        project
-      </Text>
-      <HStack gap="sm" vAlign="center">
-        <SiGithub className="h-3 w-3 shrink-0 fill-current text-zinc-500 dark:text-zinc-400" />
-        <Link
-          href="https://github.com/joshglazer/deep-cuts"
-          target="_blank"
-          isStandalone
+    <footer>
+      {session?.spotifyUserId && (
+        <Text
           type="supporting"
           size="sm"
-          hasUnderline={false}
-          color="secondary"
+          display="block"
+          justify="center"
+          className="px-6 pt-4"
         >
-          Source Code
-        </Link>
-      </HStack>
+          Listening activity may be delayed up to 15 minutes.
+        </Text>
+      )}
+      <div className="flex items-center justify-between border-t border-zinc-200 px-6 py-4 dark:border-zinc-800">
+        <Text type="supporting" size="sm">
+          A{" "}
+          <Link
+            href="https://joshglazer.com"
+            target="_blank"
+            type="inherit"
+            color="inherit"
+          >
+            Josh Glazer
+          </Link>{" "}
+          project
+        </Text>
+        <HStack gap="sm" vAlign="center">
+          <SiGithub className="h-3 w-3 shrink-0 fill-current text-zinc-500 dark:text-zinc-400" />
+          <Link
+            href="https://github.com/joshglazer/deep-cuts"
+            target="_blank"
+            isStandalone
+            type="supporting"
+            size="sm"
+            hasUnderline={false}
+            color="secondary"
+          >
+            Source Code
+          </Link>
+        </HStack>
+      </div>
     </footer>
   );
 }
